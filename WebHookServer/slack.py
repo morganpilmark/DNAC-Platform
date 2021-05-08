8786f89b679642ecfe760cc42e20789a2f9b51de
Morgan Pilmark <morgan.pilmark@jppol.dk>
2021-05-08 15:10:39 +0200

    slack module

---
+++ WebHookServer/slack.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import sys
+import os
+# hide the slack webhook url
+sys.path.append(os.path.join(os.path.dirname(__file__), "config"))
+from slack_config import webhook_url
+
+def post_slack(header,message):
+    # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook
+    slack_data = {'text': "Sup! We're hacking shit together :spaghetti:"}
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
